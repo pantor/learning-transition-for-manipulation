2f68984ecc71402b45fe89ee9c0f8b66c6d6c208
Lars Berscheid <lars.berscheid@kit.edu>
2019-08-30 11:11:50 +0200

    better data model

--- docs/script.js
+++ docs/script.js
@@ -7,7 +7,7 @@ var setCurrentStep = function(new_step, length) {
 };
 
 Vue.component('steps-carousel', {
-    props: ['length', 'name', 'estimated_rewards'],
+    props: ['name', 'steps'],
     template: '#steps-carousel-template',
     data() {
       return {
@@ -22,50 +22,48 @@ Vue.component('steps-carousel', {
 var v = new Vue({
     el: '#content',
     data: {
-      estimated_rewards: {
-        'simple-example-1': [
-          [0.999],
-          [0.996, 0.001],
-          [0.996, 0.0],
-          [0.994, 0.003],
-          [0.991, 0.003],
-          [0.987, 0.001],
-          [0.981, 0.019],
-          [0.973, 0.006],
-          [0.985, 0.028],
-          [1.0],
-        ],
-        'complex-manipulation-1': [
-          [0.975],
-          [0.972, 0.084],
-          [0.774],
-          [0.839],
-          [0.858, 0.015],
-          [0.788, 0.076],
-          [1.0],
-        ],
-        'push-try-1': [
-          [0.899],
-          [0.983, 0.047],
-          [0.969, 0.006],
-          [0.989, 0.042],
-        ],
-        'push-try-3': [
-          [0.912],
-          [0.981, 0.003],
-          [0.879],
-          [0.976, 0.007],
-          [0.985, 0.04],
-        ],
-        'bin-picking-2': [
-          [0.991],
-          [0.995, 0.001],
-          [0.981, 0.012],
-          [0.983, 0.005],
-          [0.971, 0.014],
-          [0.930, 0.037],
-          [0.949, 0.025],
-        ]
-      }
+      simple_example_1: [
+        { action_type: 1, reward: 1, estimated_reward: 0.999 },
+        { action_type: 1, reward: 1, estimated_reward: 0.996, estimated_reward_std: 0.001 },
+        { action_type: 0, reward: 1, estimated_reward: 0.996, estimated_reward_std: 0.0 },
+        { action_type: 1, reward: 1, estimated_reward: 0.994, estimated_reward_std: 0.003 },
+        { action_type: 1, reward: 1, estimated_reward: 0.991, estimated_reward_std: 0.003 },
+        { action_type: 0, reward: 1, estimated_reward: 0.987, estimated_reward_std: 0.001 },
+        { action_type: 2, reward: 1, estimated_reward: 0.981, estimated_reward_std: 0.019 },
+        { action_type: 2, reward: 1, estimated_reward: 0.973, estimated_reward_std: 0.006 },
+        { action_type: 1, reward: 1, estimated_reward: 0.985, estimated_reward_std: 0.028 },
+        { action_type: 4, reward: 1, estimated_reward: 1.0 },
+      ],
+      complex_manipulation_1: [
+        { action_type: 0, reward: 1, estimated_reward: 0.975 },
+        { action_type: 1, reward: 1, estimated_reward: 0.972, estimated_reward_std: 0.084 },
+        { action_type: 3, reward: 0.62, estimated_reward: 0.774 },
+        { action_type: 3, reward: 0.77, estimated_reward: 0.839 },
+        { action_type: 1, reward: 1, estimated_reward: 0.858, estimated_reward_std: 0.015 },
+        { action_type: 1, reward: 1, estimated_reward: 0.788, estimated_reward_std: 0.076 },
+        { action_type: 4, reward: 1, estimated_reward: 1.0 },
+      ],
+      bin_picking_2: [
+        { action_type: 1, reward: 1, estimated_reward: 0.991 },
+        { action_type: 0, reward: 1, estimated_reward: 0.995, estimated_reward_std: 0.001 },
+        { action_type: 1, reward: 1, estimated_reward: 0.981, estimated_reward_std: 0.012 },
+        { action_type: 1, reward: 1, estimated_reward: 0.983, estimated_reward_std: 0.005 },
+        { action_type: 0, reward: 1, estimated_reward: 0.971, estimated_reward_std: 0.014 },
+        { action_type: 0, reward: 1, estimated_reward: 0.930, estimated_reward_std: 0.037 },
+        { action_type: 0, reward: 1, estimated_reward: 0.949, estimated_reward_std: 0.025 },
+      ],
+      push_try_1: [
+        { action_type: 3, reward: 0.91, estimated_reward: 0.899 },
+        { action_type: 1, reward: 1, estimated_reward: 0.983, estimated_reward_std: 0.047 },
+        { action_type: 0, reward: 1, estimated_reward: 0.969, estimated_reward_std: 0.006 },
+        { action_type: 1, reward: 1, estimated_reward: 0.989, estimated_reward_std: 0.042 },
+      ],
+      push_try_3: [
+        { action_type: 3, reward: 0.84, estimated_reward: 0.912 },
+        { action_type: 1, reward: 1, estimated_reward: 0.981, estimated_reward_std: 0.003 },
+        { action_type: 3, reward: 0.89, estimated_reward: 0.879 },
+        { action_type: 1, reward: 1, estimated_reward: 0.976, estimated_reward_std: 0.007 },
+        { action_type: 1, reward: 1, estimated_reward: 0.985, estimated_reward_std: 0.04 },
+      ],
     }
 });
